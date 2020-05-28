fc64d2eeefe215c6e63eaed125e595ed9fa32dfb
WMDA <p3dh0001@gmail.com>
2020-05-28 18:01:10 +0100

    Command line options. Use -t to specify a number of topics, -c for number of comments, -config to used config.yaml file -h for help. Also will now print to terminal what topics and number of comments that are currently being searched for. Have updated the readme file as well.

--- reddata/function.py
+++ reddata/function.py
@@ -1,7 +1,27 @@
 import yaml
+import argparse
 
 def load_config(config_file = "config.yaml"):
-
     config_yml = open(config_file)
     config = yaml.load(config_yml)
     return config
+
+def get_arguments():
+    parse = argparse.ArgumentParser()
+    parse.add_argument("-t", "--topics", dest='topics', nargs='*', help='Topics in for praw to search reddit')
+    parse.add_argument("-c","--comments", dest='comments', help='Selects number of comments for praw to limit to.')
+    parse.add_argument("-config",help="Uses config.yaml file instead of providing options, doesn't take any arguments but needs config.yaml file (provided with package)",action="store_true")
+    options= parse.parse_args()
+    if options.config:
+        config = load_config()
+        options.topics=config["topics"]
+        options.comments= config["comments"]
+        return options
+    elif not options.topics or not options.comments:
+        parse.error(">> Needs topic and number of comment. Use -t and put topics and -c to put number of comments or use -config. Use -h for more information.")
+    else:
+        return options
+
+def print_output(topic,comments):
+    print('Searching reddit for','\n',topic)
+    print('Limiting comments to','\n', comments)

ba1384130c0b1679683e0899bc918769fd596207
WMDA <p3dh0001@gmail.com>
2020-06-01 16:12:16 +0100

    Added optional arguments to save file to csv and give it a name. use -csv to save to csvbut this option needs a directory. Use -n to give file a name. If no -n is used file is called reddit_database.csv. Also added warnings in if no flags are used to save file to csv

--- pipeline.py
+++ pipeline.py
@@ -11,9 +11,10 @@ This pipeline takes an a topic or topics (any number equal or greater than 1) an
 
 import reddata as rd
 import praw
+import pandas as pd
 
 options= rd.get_arguments()
-topics_list = str(options.topics)
+topics_list = options.topics
 number_comments = int(options.comments)
 
 rd.print_output(topics_list,number_comments)
@@ -23,3 +24,12 @@ reddit = praw.Reddit("reddit")
 subs_array = rd.get_subreddit_names(reddit, topics_list)
 
 database = rd.get_subreddit_data(reddit, subs_array, number_comments)
+
+if options.csv and not options.name:
+    database.to_csv("%s/reddit_database.csv" % options.csv, encoding='utf-8', index=False)
+elif options.csv and options.name:
+    database.to_csv("%s/%s.csv" % (options.csv,options.name), encoding='utf-8', index=False)
+elif options.name and not options.csv:
+    print("WARNING: File name given but directory was not given. Results were not saved to file format.",'\n',"Use -h or --help for support")
+elif not options.csv and not options.name:
+    print("WARNING: No flag to save results to a file was given." '\n',"Use -h or --help for support or go to https://github.com/WMDA/social-media-analysis")
--- reddata/function.py
+++ reddata/function.py
@@ -11,6 +11,8 @@ def get_arguments():
     parse.add_argument("-t", "--topics", dest='topics', nargs='*', help='Topics in for praw to search reddit')
     parse.add_argument("-c","--comments", dest='comments', help='Selects number of comments for praw to limit to.')
     parse.add_argument("-config",help="Uses config.yaml file instead of providing options, doesn't take any arguments but needs config.yaml file (provided with package)",action="store_true")
+    parse.add_argument("-csv", help="Saves output to CSV, needs a directory to save csv to")
+    parse.add_argument("-n","--name", dest='name', help="Gives the file a name, if this option is not used in conjunction with -csv then file will be called reddit_database")
     options= parse.parse_args()
     if options.config:
         config = load_config()
